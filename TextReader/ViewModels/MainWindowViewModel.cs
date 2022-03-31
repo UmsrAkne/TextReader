bf60923e583d31c5fd2af7e6caf4e6bb193f333e
Akn <sibata486@gmail.com>
2022-03-31 23:55:29 +0900

    add / ListView の ItemsSource を仮設定

--- TextReader/ViewModels/MainWindowViewModel.cs
+++ TextReader/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,19 @@
 ﻿namespace TextReader.ViewModels
 {
-using Prism.Mvvm;
+    using System.Collections.Generic;
+    using Prism.Mvvm;
+    using TextReader.Models;
 
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
+
+        private List<IText> texts = new List<IText>()
+        {
+            new PlainText("sampleText1"),
+            new PlainText("sampleText2"),
+            new PlainText("sampleText3")
+        };
 
         public MainWindowViewModel()
         {
@@ -15,5 +24,7 @@ using Prism.Mvvm;
             get { return title; }
             set { SetProperty(ref title, value); }
         }
+
+        public List<IText> Texts { get => texts; set => SetProperty(ref texts, value); }
     }
 }
