fa2fdb198815220035e985ea778108a7d3c2aa7c
Akn <sibata486@gmail.com>
2022-04-02 23:40:52 +0900

    add / DBQueryer のテストを追加

---
+++ TextReaderTests/Models/DBQueryerTests.cs
@@ -0,0 +1,59 @@
+﻿namespace TextReader.Models.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DBQueryerTests
+    {
+        [TestMethod]
+        public void GetTextsTest()
+        {
+            DBQueryer queryer = new DBQueryer();
+            List<TextRecord> records = new List<TextRecord>();
+
+            records.Add(new TextRecord()
+            {
+                Title = "testTitle",
+                CreationDateTime = new DateTime(2022,12,10),
+                Text = "text1",
+                Index = 1
+            });
+
+            records.Add(new TextRecord()
+            {
+                Title = "testTitle",
+                CreationDateTime = new DateTime(2022,12,10),
+                Text = "text2",
+                Index = 2
+            });
+
+            records.Add(new TextRecord()
+            {
+                Title = "testTitle",
+                CreationDateTime = new DateTime(2022,12,10),
+                Text = "text3",
+                Index = 3
+            });
+
+            records.Add(new TextRecord()
+            {
+                Title = "testTitle",
+                CreationDateTime = new DateTime(2022,12,11),
+                Text = "text1",
+                Index = 1
+            });
+
+            queryer.Target = records;
+
+            var texts = queryer.GetTexts("testTitle", new DateTime(2022, 12, 10)).ToList();
+
+            Assert.AreEqual(texts.Count(), 3);
+            Assert.AreEqual(texts[0].Text, "text1");
+            Assert.AreEqual(texts[1].Text, "text2");
+            Assert.AreEqual(texts[2].Text, "text3");
+        }
+    }
+}
