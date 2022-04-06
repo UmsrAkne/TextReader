0a544508f0e540d3b8a74d31136e9e7ece89d5c9
Akn <sibata486@gmail.com>
2022-04-06 23:39:42 +0900

    add / TextDBContext のテストクラスを追加

---
+++ TextReaderTests/Models/DBs/TextDBContextTests.cs
@@ -0,0 +1,14 @@
+﻿namespace TextReader.Models.DBs.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TextDBContextTests
+    {
+        [TestMethod]
+        public void GetTextsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
