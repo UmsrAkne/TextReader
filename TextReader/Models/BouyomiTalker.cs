b99dcca4f462debf7ca111d8874be00debe821bf
Akn <sibata486@gmail.com>
2022-04-10 23:26:06 +0900

    add / 棒読みちゃんの読み上げを操作するクラスを追加

---
+++ TextReader/Models/BouyomiTalker.cs
@@ -0,0 +1,21 @@
+﻿namespace TextReader.Models
+{
+    using System.Diagnostics;
+
+    public class BouyomiTalker
+    {
+        public void Talk(string str)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = @"BouyomiChan\RemoteTalk\RemoteTalk.exe";
+            process.StartInfo.RedirectStandardOutput = true;
+
+            // 標準出力を使うためには false にセットする必要があるみたい
+            process.StartInfo.UseShellExecute = false;
+
+            string commandText = $"/Talk {str}";
+            process.StartInfo.Arguments = $"{commandText}";
+            process.Start();
+        }
+    }
+}
