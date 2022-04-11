32d6553a7444c074f24e975f6d001f1b2bf20dfd
Akn <sibata486@gmail.com>
2022-04-11 23:02:06 +0900

    add / テキストを読み上げるプレイヤークラスを追加

---
+++ TextReader/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// インスタンス内に IPlayer を実装したクラスを内蔵。
+/// それを使って入力されたテキスト（リスト）の読み上げを行うクラスです。
+/// </summary>
+namespace TextReader.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Player
+    {
+    }
+}
