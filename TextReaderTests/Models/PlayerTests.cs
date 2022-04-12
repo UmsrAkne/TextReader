e3c789d795f447238835d3576e4f916965aaefa2
Akn <sibata486@gmail.com>
2022-04-12 22:30:36 +0900

    add / Player のテストクラスと、ITalker 実装のダミークラスを追加

---
+++ TextReaderTests/Models/PlayerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TextReader.Models.Tests
+{
+
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void PlayTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TextReaderTests/Models/Talkers/DummyTalker.cs
@@ -0,0 +1,20 @@
+﻿namespace TextReaderTests.Models.Talkers
+{
+    using System;
+    using TextReader.Models.Talkers;
+
+    public class DummyTalker : ITalker
+    {
+        public event EventHandler TalkStopped;
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Talk(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
