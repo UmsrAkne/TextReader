983a8bb4cc9613ec37266ebfd3c76e232650811a
Akn <sibata486@gmail.com>
2022-04-14 22:55:13 +0900

    add / 読み上げ時のウェイト機能を実装
    読み上げるテキスト中に空行が入った場合、待機時間が入るよう変更

--- TextReader/Models/Talkers/BouyomiTalker.cs
+++ TextReader/Models/Talkers/BouyomiTalker.cs
@@ -8,10 +8,12 @@
     public class BouyomiTalker : ITalker
     {
         private DispatcherTimer timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(500) };
+        private DispatcherTimer waitTimer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(3000) };
 
         public BouyomiTalker()
         {
             timer.Tick += Timer_Tick;
+            waitTimer.Tick += Wait; 
         }
 
         public event EventHandler TalkStopped;
@@ -23,8 +25,15 @@
 
         public void Talk(string str)
         {
-            ExecuteRemoteTalk($"/Talk {str}");
-            timer.Start();
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                waitTimer.Start();
+            }
+            else
+            {
+                ExecuteRemoteTalk($"/Talk {str}");
+                timer.Start();
+            }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -39,6 +48,12 @@
                 timer.Stop();
                 TalkStopped?.Invoke(this, EventArgs.Empty);
             }
+        }
+
+        private void Wait(object sender, EventArgs e)
+        {
+            waitTimer.Stop();
+            TalkStopped?.Invoke(this, EventArgs.Empty);
         }
 
         private Process ExecuteRemoteTalk(string argument)
