8f9e59e4e9993e65b020a81fcb24bcc446bdbfa7
Akn <sibata486@gmail.com>
2022-04-24 23:07:31 +0900

    add / AzureTalker の機能を実装(途中)

--- TextReader/Models/Talkers/AzureTalker.cs
+++ TextReader/Models/Talkers/AzureTalker.cs
@@ -3,10 +3,10 @@
     using System;
     using System.IO;
     using System.Threading.Tasks;
-    using TextReader.Models.DBs;
-    using TextReader.Models.Talkers;
     using Microsoft.CognitiveServices.Speech;
     using Microsoft.CognitiveServices.Speech.Audio;
+    using TextReader.Models.DBs;
+    using TextReader.Models.Talkers;
     // using WMPLib;
 
     public class AzureTalker : ITalker
@@ -35,22 +35,17 @@
 
         public string OutputFileName { get; private set; }
 
-        public bool CanPlay => throw new NotImplementedException();
+        public bool CanPlay => true;
 
-        public int TalkSpeed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int TalkSpeed { get; set; }
 
-        public int MaxTalkSpeed => throw new NotImplementedException();
+        public int MaxTalkSpeed => 100;
 
-        public int MinTalkSpeed => throw new NotImplementedException();
+        public int MinTalkSpeed => 0;
 
-        public int Volume { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Volume { get; set; }
 
         // private WindowsMediaPlayer WMP { get; } = new WindowsMediaPlayer();
-
-        public async void SSMLTalk(string ssmlText)
-        {
-            await Talk(ssmlText);
-        }
 
         public void Stop()
         {
@@ -59,28 +54,28 @@
 
         private async Task Talk(string ssml)
         {
-            // string key = Environment.GetEnvironmentVariable("Microsoft_Speech_Secret_key");
+            string key = Environment.GetEnvironmentVariable("Microsoft_Speech_Secret_key");
 
-            // var config = SpeechConfig.FromSubscription(key, "japaneast");
-            // config.SpeechSynthesisLanguage = "ja-JP";
-            // config.SpeechSynthesisVoiceName = "ja-JP-KeitaNeural";
+            var config = SpeechConfig.FromSubscription(key, "japaneast");
+            config.SpeechSynthesisLanguage = "ja-JP";
+            config.SpeechSynthesisVoiceName = "ja-JP-KeitaNeural";
 
-            // OutputFileName = DateTime.Now.ToString("yyyyMMddHHmmssff") + ".wav";
-            // var audioConfig = AudioConfig.FromWavFileOutput($"{outputDirectoryInfo.Name}\\{OutputFileName}");
+            OutputFileName = $"{DateTime.Now.ToString("yyyyMMddHHmmssff")}.wav";
+            var audioConfig = AudioConfig.FromWavFileOutput($"{outputDirectoryInfo.Name}\\{OutputFileName}");
 
-            // using (var synthesizer = new SpeechSynthesizer(config, audioConfig))
-            // {
-            //     var ssmlGen = new AzureSSMLGen();
-            //     await synthesizer.SpeakSsmlAsync(ssml);
-            // }
+            using (var synthesizer = new SpeechSynthesizer(config, audioConfig))
+            {
+                // await synthesizer.SpeakSsmlAsync(ssml);
+                await synthesizer.SpeakTextAsync(ssml);
+            }
 
             // WMP.URL = $"{outputDirectoryInfo.Name}\\{OutputFileName}";
             // WMP.controls.play();
         }
 
-        public void Talk(TextRecord textRecord)
+        public async void Talk(TextRecord textRecord)
         {
-            throw new NotImplementedException();
+            await Talk(textRecord.Text);
         }
     }
 }
