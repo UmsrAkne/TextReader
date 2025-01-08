c428e1cf1895fe34c16850add02895d3e07ba79f
Akn <sibata486@gmail.com>
2025-01-08 15:35:44 +0900

    refactoring /

--- TextReader/Models/NumberFormatConverter.cs
+++ TextReader/Models/NumberFormatConverter.cs
@@ -13,7 +13,10 @@ namespace TextReader.Models
             {
                 // "0" を指定回数分詰め込んだ配列を作成
                 var zeros = Enumerable.Range(1, int.Parse((string)parameter)).Select(n => "0").ToArray();
-                return string.Format("{0:" + string.Join(string.Empty, zeros) + "}", (int)value);
+                if (value != null)
+                {
+                    return string.Format("{0:" + string.Join(string.Empty, zeros) + "}", (int)value);
+                }
             }
 
             return value != null
